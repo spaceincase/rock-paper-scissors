f882d571ba4f4cafd4b75fa9f825dfd2d1b338cb
d3vi4nt1337 <bradlypetrsn1991@gmail.com>
2019-10-04 10:24:18 -0500

    finished the UI

--- javascript.js
+++ javascript.js
@@ -17,19 +17,45 @@ function computerPlay() {
 }
 
 function playRound(e) {
+
+  e.target.classList.add('animated')
   var playerSelection = e.toElement.id
   var computerSelection = computerPlay();
   if (playerSelection == computerSelection) {
-    console.log("Draw")
+    setMessage("Draw")
   } else if (playerSelection == "rock" && computerSelection == "paper" ||
             playerSelection == "paper" && computerSelection == "scissors" ||
             playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("Lose")
+    setMessage("Lose")
+    computerScore += 1
+    setScore()
   } else {
-    console.log("Win")
+    setMessage("Win")
+    playerScore += 1
+    setScore()
   }
 }
 
+function setScore() {
+  score.innerHTML = "Score: " + playerScore + " to " + computerScore
+}
+
+function setMessage(message) {
+  query = document.querySelector('.message h3')
+  query.innerHTML = message
+}
+
+function removeTransition(e) {
+    
+    e.target.classList.remove('animated');
+  }
+
+
 const btns = Array.from(document.querySelectorAll('.btn'))
 btns.forEach(btn => btn.addEventListener('click', playRound))
+btns.forEach(btn => btn.addEventListener('transitionend', removeTransition))
 
+var computerScore = 0
+var playerScore = 0
+const score = document.querySelector('.score h3')
+setScore()
